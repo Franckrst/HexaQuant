2f4625dd5ee22502cda3c523c4cd380f46c675e2
franck <franck.rst@gmail.com>
2020-11-04 15:05:45 +0100

    fix

--- Prefabs/Mannequiny.gd
+++ Prefabs/Mannequiny.gd
@@ -2,7 +2,7 @@ extends KinematicBody
 
 var path = []
 var path_ind = 0
-const move_speed = 5
+const move_speed = 8
 onready var amap = get_parent()
 func _ready():
 	add_to_group("units")
@@ -13,7 +13,7 @@ func _physics_process(delta):
 		if Vector2(move_vec.x,move_vec.z).length() < 0.1:
 			path_ind += 1
 		else:
-			if $RayCast.is_colliding():
+			if is_on_floor():
 				move_vec[1] = 0;
 			var toto = move_vec.normalized()*move_speed
 			#move_and_slide_with_snap(move_vec.normalized()*move_speed,Vector3.DOWN,Vector3.DOWN,true)
@@ -24,7 +24,6 @@ func _physics_process(delta):
 			print(str(self.get_rotation()))
 	else:
 		$AnimationPlayer.stop()
-#move_and_slide_with_snap(move_vec.normalized() * move_speed, Vector3(0, -1, 0),Vector3(0, 1, 0))
 
 func move_to(target_pos):
 	print("-------+->"+str(global_transform.origin))
